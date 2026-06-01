58c190997b17fab81694a76dad028b6ce78ef3bc
Simon Porritt <simon@jsplumbtoolkit.com>
2026-06-01 17:27:45 +1000

    initial import

---
+++ ABOUT.md
@@ -0,0 +1,18 @@
+### Summary
+This demo demonstrates bar and column charts in a Svelte application using VisuallyJS.
+
+### Components Used
+- `BarChartComponent`: Used for rendering horizontal bar charts.
+- `ColumnChartComponent`: Used for rendering vertical column charts.
+
+### Component Options
+Both components use the following props:
+- `options`: A configuration object for the chart.
+- `className`: Optional CSS class name for the chart container.
+
+### Stylesheet Requirement
+The `visuallyjs.css` stylesheet must be included for the components to function and display correctly.
+
+```css
+@import "@visuallyjs/browser-ui/css/visuallyjs.css";
+```
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { BarChartComponent, ColumnChartComponent } from "@visuallyjs/browser-ui-svelte";
+    import { charts } from "./examples";
+</script>
+
+<div class="vjs-bar-column-chart">
+    {#each charts as chart}
+        <div class="vjs-chart-example">
+            <div class="vjs-chart-example-title">{chart.desc}</div>
+            {#if chart.comments}
+                <div class="vjs-chart-example-comments">{chart.comments}</div>
+            {/if}
+            {#if chart.type === 'bar'}
+                <BarChartComponent className="vjs-chart-example-container" options={chart.options} />
+            {:else if chart.type === 'column'}
+                <ColumnChartComponent className="vjs-chart-example-container" options={chart.options} />
+            {/if}
+        </div>
+    {/each}
+</div>
